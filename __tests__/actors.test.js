55b72a8ab1f3ba8cfeb1927aaf31e9ecfae6129a
Ian Andrewson <ianandrewson@gmail.com>
2020-01-06 19:09:50 -0800

    actor routes test implemented. Ready to TDD actor routes

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -5,6 +5,11 @@ const app = require('../lib/app.js');
 const mongoose = require('mongoose');
 const Actor = require('../lib/models/Actor.js');
 
+//get: _id, name
+//get/:id: name, dob, pob, films: [{ id, title, released }]
+//post
+//
+
 describe('actor routes tests', () => {
 
   beforeAll(() => {
@@ -19,5 +24,68 @@ describe('actor routes tests', () => {
     return mongoose.connection.close();
   });
 
-  it('')
+  it('should be able to post a new actor', async() => {
+    let date = new Date('4/16/1889')
+    const actor = await Actor.create({
+      name: 'Charlie Chaplin',
+      dob: date,
+      pob: 'London, England'
+    });
+    return request(app)
+      .post('/api/v1/actors')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: actor._id.toString(),
+          name: 'Charlie Chaplin',
+          dob: date,
+          pob: 'London, England',
+          __v: 0
+        });
+      });
+  });
+
+  //update to also send associated films
+  it('should be able to get an actor by ID', async() => {
+    const date = new Date('4/16/1889');
+    const actor = await Actor.create({
+      name: 'Charlie Chaplin',
+      dob: date,
+      pob: 'London, England'
+    });
+    return request(app)
+      .get(`/api/v1/actors/${actor._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: actor._id.toString(),
+          name: 'Charlie Chaplin',
+          dob: date,
+          pob: 'London, England',
+          __v: 0
+        });
+      });
+  });
+
+  it('should be able to get all actors', async() => {
+    const date1 = new Date('1/1/1990');
+    const date2 = new Date('1/5/1975');
+    const date3 = new Date('5/23/1964');
+    const actors = await Actor.create([
+      { name: 'Some Dude', dob: date1, pob: 'Copenhagen, Denmark'},
+      { name: 'Other Person', dob: date2, pob: 'Nome, Alaska' },
+      { name: 'A Fair lady', dob: date3, pob: 'Portland, Oregon' }
+    ]);
+    return request(app)
+      .get('/api/v1/actors')
+      .then(res => {
+        actors.forEach(actor => {
+          expect(res.body).toContainEqual({
+            _id: actor._id.toString(),
+            name: actor.name,
+            dob: actor.dob,
+            pob: actor.lob,
+            __v: 0
+          });
+        });
+      });
+  });
 });
